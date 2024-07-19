5150b1087b60afbb2db66649bae672ae2e70d548
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-07-19 17:21:20 +0530

    Deployed FrontEnd

---
+++ CONNECTO/Client/src/Pages/LandingPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="center-content">
+        <h1>WELCOME TO CONNECTO</h1>
+        <h2>Connecting you to the knowledge and connections you need</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
