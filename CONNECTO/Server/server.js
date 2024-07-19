87b77fbd88bed835276d1822e40771fe23ad5dbf
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-07-19 15:51:42 +0530

    done

---
+++ CONNECTO/Server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+const port = process.env.PORT || 5001;
+
+
+dotenv.config();
+
+
+app.use(express.json());
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Basic route
+app.get('/', (req, res) => {
+  res.send('Welcome to CONNECTO!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
