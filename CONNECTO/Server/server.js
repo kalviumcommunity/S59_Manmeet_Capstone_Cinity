97c301080c4c8e589f004273026f275dcd692020
ManmeetSingh777 <gmanmeetsingh888@gmail.com>
2024-07-23 14:40:38 +0530

    suggestion implemented

--- CONNECTO/Server/server.js
+++ CONNECTO/Server/server.js
@@ -5,17 +5,13 @@ const User = require('./models/User');
 const app = express();
 const port = process.env.PORT || 5001;
 
-
 dotenv.config();
 
-
 app.use(express.json());
-
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
-
 
 app.get('/', (req, res) => {
   res.send('Welcome to CONNECTO!');
@@ -29,7 +25,10 @@ app.post('/users', async (req, res) => {
     await user.save();
     res.status(201).send(user);
   } catch (err) {
-    res.status(400).send(err);
+    res.status(400).send({
+      message: 'Error creating user',
+      error: err.message
+    });
   }
 });
 
@@ -39,7 +38,17 @@ app.get('/users', async (req, res) => {
     const users = await User.find();
     res.status(200).json(users);
   } catch (err) {
-    res.status(500).send(err);
+    if (err instanceof mongoose.Error) {
+      res.status(500).send({
+        message: 'Database error occurred',
+        error: err.message
+      });
+    } else {
+      res.status(500).send({
+        message: 'An unknown error occurred',
+        error: err.message
+      });
+    }
   }
 });
 
